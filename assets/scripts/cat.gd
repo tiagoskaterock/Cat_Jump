ed78c042813d1977fc0e2847d06b0b935461d8fa
tiagoskaterock <tiagolemespalhano@gmail.com>
2020-04-19 11:19:16 -0300

    gato se move

--- assets/scripts/cat.gd
+++ assets/scripts/cat.gd
@@ -13,6 +13,32 @@ func _ready() -> void:
 	set_process_input(true)
 
 func _physics_process(delta: float) -> void:
-	pass
+	linear_velocity.y += + GRAVITY * delta
+	if is_alive:
+		if is_on_floor():
+			linear_velocity.x = 0
+			if is_jumping:
+				linear_velocity.y = JUMP_FORCE
+		elif is_moving:
+			linear_velocity.x = SPEED
+		else:
+			linear_velocity.x = 0
 	
+	is_jumping = false
 
+	if position.y > get_viewport_rect().size.y:
+		morte()
+		
+	linear_velocity = move_and_slide(linear_velocity, Vector2(0, -1))
+			
+func morte():
+	print("morreu")
+	
+# touch screen
+func _input(event: InputEvent) -> void:
+	if event is InputEventScreenTouch:
+		if event.pressed:
+			is_moving = true and is_on_floor()
+			is_jumping = true
+		else:
+			is_moving = false

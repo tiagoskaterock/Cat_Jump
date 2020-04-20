0ce341bc83b72c978830ba3f6f3b48ef15024c9b
tiagoskaterock <tiagolemespalhano@gmail.com>
2020-04-19 22:28:15 -0300

    node_group.gd atualizado mas ainda não cria a nova plataforma

--- assets/scripts/ground_group.gd
+++ assets/scripts/ground_group.gd
@@ -1,4 +1,13 @@
 extends Node2D
 
+export (PackedScene) var ground
+
 func _ready():
 	pass
+
+func generate_ground(current_position_x):
+	var new_ground = ground.instance()
+	var new_position = Vector2(650 + current_position_x, 1150)
+	
+	new_ground.position = new_position
+	add_child(new_ground)

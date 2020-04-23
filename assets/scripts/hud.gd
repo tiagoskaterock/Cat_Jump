9bbee14c15bb5ca2f4b85fab8d9fe2f08899242c
tiagoskaterock <tiagolemespalhano@gmail.com>
2020-04-23 20:58:08 -0300

    botão restart funcionando

--- assets/scripts/hud.gd
+++ assets/scripts/hud.gd
@@ -7,9 +7,12 @@ func count_score():
 	$score.text = str(points)
 
 func game_over():
-	print("game_over")
 	$score.hide()
 	$game_over/animation.play("game_over")
 	$game_over/last_score.text = str(points)
 	$game_over/best_score.text = str(points)
 	
+
+
+func _on_restart_pressed() -> void:
+	get_tree().reload_current_scene()
